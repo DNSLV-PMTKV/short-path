c20fca4a588e11316ff2d7decf4b6cad8743a179
DNSLV-PMTKV <pometkov.d@gmail.com>
2019-02-08 18:28:35 +0200

    My spaghetti are delicious

--- shortPath/gui.py
+++ shortPath/gui.py
@@ -1,18 +1,21 @@
 from PIL import Image, ImageDraw
-from tkinter import Tk, Canvas, Button
+from tkinter import Tk, Canvas, Button, Label
 from path import Point, PathFinder
 
-WIDTH = 250
-HEIGHT = 250
-PIXEL = 25
+WIDTH = 300
+HEIGHT = 300
+PIXEL = 30
 
 
-class grid():
+class grid:
     def __init__(self, master, width, height, *argv, **kwargs):
         self.grid = Canvas(master, width=width, height=height, *argv, **kwargs)
         self.grid.pack()
         self.image = Image.new('RGB', (width, height), 'white')
         self.draw = ImageDraw.Draw(self.image)
+
+        self.hasStart = False
+        self.hasEnd = False
 
         self.grid.bind('<Button-2>', self.drawObstacle)
         self.grid.bind('<B2-Motion>', self.drawObstacle)
@@ -34,17 +37,21 @@ class grid():
 
     def drawStartPoint(self, event):
         x, y = event.x // PIXEL, event.y // PIXEL
-        self.grid.create_rectangle(
-            x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL, fill='blue')
-        self.draw.rectangle(
-            [x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL], fill='blue')
+        if not self.hasStart:
+            self.grid.create_rectangle(
+                x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL, fill='blue')
+            self.draw.rectangle(
+                [x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL], fill='blue')
+            self.hasStart = True
 
     def drawEndPoint(self, event):
         x, y = event.x // PIXEL, event.y // PIXEL
-        self.grid.create_rectangle(
-            x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL, fill='red')
-        self.draw.rectangle(
-            [x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL], fill='red')
+        if not self.hasEnd:
+            self.grid.create_rectangle(
+                x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL, fill='red')
+            self.draw.rectangle(
+                [x * PIXEL, y * PIXEL, x * PIXEL + PIXEL, y * PIXEL + PIXEL], fill='red')
+            self.hasEnd = True
 
     def cellColor(self, x, y):
         r, g, b = self.image.getpixel((x+1, y+1))
@@ -52,8 +59,6 @@ class grid():
 
     def toArray(self):
         arr = []
-        start = []
-        end = []
         for x in range(0, WIDTH, PIXEL):
             arr.append([])
             for y in range(0, HEIGHT, PIXEL):
@@ -63,37 +68,45 @@ class grid():
                 if(self.cellColor(x, y) == (255, 0, 0)):
                     end = [x, y]
                 if(self.cellColor(x, y) == (0, 0, 0)):
-                    arr[x][y] = '#'
+                    arr[x // PIXEL][y // PIXEL] = '#'
+        print(arr, start, end)
         return arr, start, end
 
     def getShortestPath(self):
         field, startXY, endXY = self.toArray()
-        print(field, startXY, endXY)
-        startPoint = Point(startXY[0], startXY[1])
-        print(startPoint)
-        endPoint = Point(endXY[0], endXY[1])
-        print(endPoint)
-        a = PathFinder(field)
-        return a.shortest_path(startPoint, endPoint)
+        if startXY and endXY:
+            startPoint = Point(startXY[0] // PIXEL, startXY[1] // PIXEL)
+            endPoint = Point(endXY[0] // PIXEL, endXY[1] // PIXEL)
+            a = PathFinder(field)
+            return a.shortest_path(startPoint, endPoint)
+        else:
+            return None
 
     def drawPath(self):
-        shortesPath = list(self.getShortestPath())
+        shortesPath = self.getShortestPath()
         if shortesPath:
             for i in shortesPath:
                 self.grid.create_rectangle(
-                    i[0] * PIXEL, i[1] * PIXEL, i[0] * PIXEL + PIXEL, i[1] * PIXEL + PIXEL, fill='orange')
+                    i.x * PIXEL, i.y * PIXEL, i.x * PIXEL + PIXEL, i.y * PIXEL + PIXEL, fill='orange')
                 self.draw.rectangle(
-                    [i[0] * PIXEL, i[1] * PIXEL, i[0] * PIXEL + PIXEL, i[1] * PIXEL + PIXEL], fill='orange')
+                    [i.x * PIXEL, i.y * PIXEL, i.x * PIXEL + PIXEL, i.y * PIXEL + PIXEL], fill='orange')
+
+    def clearAll(self):
+        self.grid.delete('all')
+        self.drawGrid()
+        self.hasStart = False
+        self.hasEnd = False
 
 
 app = Tk()
 
 layout = grid(app, width=WIDTH, height=HEIGHT, bg='white')
 layout.drawGrid()
-print(layout.toArray())
-layout.getShortestPath()
 
-btn = Button(app, text='find', command=layout.drawPath())
+btn = Button(app, text='find', command=layout.drawPath)
 btn.pack()
+
+btn2 = Button(app, text='restart', command=layout.clearAll)
+btn2.pack()
 
 app.mainloop()
